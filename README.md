2200d750b1f578c695714ef493faf110e7912759
ryangilmour <53762848+ryangilmour@users.noreply.github.com>
2020-02-07 10:25:50 +0000

    Updated readme and model
    New model to derive a second order approximation of the system and begin PID tuning via the Ziegler Nichols method.

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Digital DC/DC control
 
+This model is written in Octave.
+
+Some preliminary documentation of the approach can be found [here](http://learn.libre.solar/b/dc-control/development/digital_control.html). 
+
 Modeling and design of the DC/DC power stage in e.g. Libre Solar charge controllers for improved control in nanogrid and/or MPPT applications.
 
-This simulation is still work in progress. It is currently based on Matlab, but should be converted to GNU Octave in the future.
+The script models the plant and divider circuitry using the state space method. From analysis of the zeros and poles, a second order approximation of the system is used (since the dominant response is second order). The second and third order responses are compared to show this is a valid approximation. 
 
-Some preliminary documentation of the approach can be found [here](http://learn.libre.solar/b/dc-control/development/digital_control.html).
+Ziegler Nichols tuning techniques are applied using the rules here:  http://faculty.mercer.edu/jenkins_he/documents/TuningforPIDControllers.pdf
+
+However, the output of this does not provide an improved step response. 
+
+**Next steps** 
+
+Calculate the PID parameters based on: The ultimate gain method OR the pole placement method.  
+
+NOTE: There is a legacy issue (from the Matlab implementation) surrounding the use of the stepinfo function. A GNU Octave alternative has been proposed [here](https://lists.gnu.org/archive/html/help-octave/2015-02/msg00023.html), however this is not tested and verified. So the use of this for calculating the bandwidth of the system is omitted. 
--- discrete_time_model.m
+++ discrete_time_model.m
@@ -1,5 +1,9 @@
+%% Load necessary packages
+pkg load control;
+pkg load signal;
 clc;
 clear;
+close all;
 grid on;
 
 %% Plant transfer function (Continous time domain):
@@ -10,22 +14,6 @@ grid on;
     % d    - control signal
     % vout - ouput of the plant
     % x    - state variable matrix
-
-
-% example:
-
-    % Vout = 1;      % outputof the power stage
-    % Vg = 10;        % input to power stage
-    % fs = 500e3;      % switcing freq
-    % Ts = 1/fs;      % switiching period
-    % Cap = 800e-6;   % Cap in power plant
-    % Resr = 2e-3;   % equivalent series resistance of cap
-    % L = 1e-6;      % Ind in power plant
-    % RL = 8.6e-3;     % series resistance of inductor
-    % R = 10;         % Load resistance in power plant
-    % Duty = Vout/Vg;    % steady state Duty cycle
-    % td = 0.25*Ts + Duty*Ts; % not sure why need to be checked
-
 
 % Our System:
 
@@ -40,7 +28,6 @@ grid on;
     R = 12;         % Load resistance in power plant
     Duty = 12/28;   % steady state Duty cycle
     td = 0.25*Ts + Duty*Ts;     % delay in A/D + Compenstor + Modulator (DTs < td < Ts)
-
 
 % Plant state space modelling:
 
@@ -59,133 +46,304 @@ grid on;
     I  = eye(2);
     plant = ss(A1,B1,C,D);
     [n_plant,d_plant] = ss2tf(A1,B1,C,D);
-    plant_tf = tf(n_plant,d_plant)
+    plant_tf = tf(n_plant,d_plant);
+
+
+    %% Exponentials are not handled by Octave control package to include a delay
+    %% Using the Pade approximation for delay seems to be the best work around
+    %% Pade (Octave): https://octave.sourceforge.io/octave/function/padecoef.html
+    %% Pade (Wiki): https://en.wikipedia.org/wiki/Pad%C3%A9_table
+
+ %TO BE IMPLEMENTED
+
 
 % plotting Step Response of Plant:
 
     figure(1);
-    subplot(2,1,1);
     step(plant);
-    title('Step Response of the Plant');
-    plant_info = stepinfo(plant);
-    [Wn_plant,DR_plant,Poles_plant] = damp(plant);
-
-
-% plotting Bode plot for Plant:
-
-    subplot(2,1,2);
-    plant_plot = bodeplot(plant);
-    setoptions(plant_plot,'FreqUnits','Hz','PhaseVisible','on');
-    title('Bode diagram of the Plant(continous model)');
-    [Gm_Plant,Pm_Plant,Wcg_Plant,Wcp_Plant] = margin(plant);
-
- % Calculation of Bandwidth of the Plant:
-
-    temp1 = sqrt((4*(DR_plant(1))^4) - (4*(DR_plant(1))^2) +2);
-    temp2 =  1 - (2*(DR_plant(1))^2);
-    temp3 = 4/(plant_info.SettlingTime*(DR_plant(1)));
-    BW_plant = temp3*sqrt(temp1 + temp2)
-
-
-% Checking stability condition:
-
-    % eig(A);
-    % if(isstable(plant))
-    %     disp('Plant is stable');
-    % else
-    %     disp('Plant is unstable');
-    % end
-
-%% Direct conversion to Discrete time domain:
-
-    dis = c2d(plant_tf,Ts);
-
-% plotting Step Response of Direct discrete conversion:
-
-    figure(2);
-    subplot(2,1,1);
-    step(dis);
-    dis_info = stepinfo(dis);
-    title('Step Response of the Plant(Direct discrete conversion)');
-
-%plotting Bode plot for Plant(Direct discrete conversion);
-
-    subplot(2,1,2);
-    dis_plot = bodeplot(dis);
-    setoptions(dis_plot,'FreqUnits','Hz','PhaseVisible','on');
-    title('Bode diagram of the Plant(Direct discrete conversion)');
-
+    title('Step Response of the Plant only');
 
 % Calculation of Bandwidth of the Plant:
 
-    t1 = sqrt((4*(DR_plant(1))^4) - (4*(DR_plant(1))^2) +2);
-    t2 =  1 - (2*(DR_plant(1))^2);
-    t3 = 4/(dis_info.SettlingTime*(DR_plant(1)));
-    BW_dis = t3*sqrt(t1 + t2)
+    %Stepinfo function now available based on funtion forum input here: https://lists.gnu.org/archive/html/help-octave/2015-02/msg00023.html
+    % Function is not verified as working.
+##    plant_info = stepinfo(plant);
+##    [Wn_plant,DR_plant,Poles_plant] = damp(plant);
 
-%% Control-to-output-voltage transfer function (Discrete time domain):
+ % BEWARE 0 Stepinfo function is providing incorrect information.
+##    temp1 = sqrt((4*(DR_plant(1))^4) - (4*(DR_plant(1))^2) +2);
+##    temp2 =  1 - (2*(DR_plant(1))^2);
+##
+##    %%Depends on plant info - using current stepinfo command (not tested)
+##    temp3 = 4/(plant_info.SettlingTime*(DR_plant(1)));
+##    BW_plant = temp3*sqrt(temp1 + temp2)
 
-% Literature : Applying Digital Technology to PWM Control-Loop Designs
-    % Discrete time modelling (Trailing Edge DPWM and sampling at interval 2(DTs < td < Ts)):
+% plotting Bode plot for Plant:
 
-        % phi   = (exp(A2*(Ts-td)))*(exp(A1*Duty*Ts))*exp(A2*(td-Duty*Ts));
-        % alpha = (B1 - B2)*Vg;
-        % gamma = (exp(A2*(Ts-td)))*alpha*Ts;
-        % z     = tf('z');
-        % Gvd   = (C* inv(I-((1/z)*phi))*gamma)/z
-
-
-% Literature (Small-Signal Discrete-Time Modeling of Digitally Controlled PWM Converters)
-    % Direct equation:
-
-     z      = tf('z');
-     Num    = z + (Ts/(Ts- td + Cap*Resr))*((Resr/R)-((Cap*Resr)/Ts)- (((Ts-td)*Resr)/L)+(td/Ts));
-     Dem    = z^2 - (2 - (Ts/(R*Cap)))*z + (1 - Ts/(R*Cap) + Ts^2/(L*Cap));
-     Gvd2   = ((Vg*Ts*(Ts- td + Cap*Resr))*Num)/(L*Cap*Dem)
+    %% Use bode (Octave) vs bodeplot (Matlab) , similar functionality
+    figure(2)
+    bode(plant);
+    title('Bode diagram of the Plant only (continous model)');
+    [Gm_Plant,Pm_Plant,Wcg_Plant,Wcp_Plant] = margin(plant);
 
 
-% plotting Step Response of Plant(Gvd2):
-
-    figure(3);
-    %subplot(2,1,1);
-    step(Gvd2)
-    title('Step Response of the control-to-output-voltage transfer function');
-    % Gvd_info = stepinfo(Gvd2);
-    %[Wn_Gvd,DR_Gvd,Poles_Gvd] = damp(Gvd2);
-
-% plotting Bode plot for Gvd2:
-
-%     subplot(2,1,2);
-%     Gvd2_plot = bodeplot(Gvd2);
-%     %setoptions(Gvd2_plot,'FreqUnits','Hz','PhaseVisible','on');
-%     title('Bode diagram of the control-to-output-voltage transfer function');
-%     %[Gm_Gvd,Pm_Gvd,Wcg_Gvd,Wcp_Gvd] = margin(Gvd2);
 
 %% Divider circuitry:
-% Voltage is divided to ADC full scale by divider circuit:
 
-    % R1 = 100e3;
-    % R2 = 5.6e3;
-    % Cz = 1e-6;
-    % Rp = (R1*R2)/(R1+R2);
-    % Cp = 1/(2*pi*fs*Rp);
+% Documentation here: https://learn.libre.solar/b/dc-control/development/digital_control.html#modelling-the-digital-controller
+% Voltage is divided to ADC full scale by divider circuit:
+     R1 = 100e3;
+     R2 = 5.6e3;
+     Cz = 0;%ORIGIRNAL CODE:%Cz = 1e-6;
+     %Typically not populated as this is not a programmable compensation zero
+     %Hardware currently does not have Cz.
+     Rp = (R1*R2)/(R1+R2);
+     Cp = 10e-9; %ORIGINAL CODE: Cp = 1/(2*pi*fs*Rp);
+     % Cp = 10nF is as implemented in hardware
 
 % Gain of the Divider:
 
-    % Kdiv = R2/(R1+R2);
+     Kdiv = R2/(R1+R2);
+     Vsense = Vout * Kdiv; %The sensed voltage in for control loop. Max voltage is 3.3V.
 
 % Transfer function of the Divider circuit:
 
-    % G1 = tf([R1*Cz 1],[Kdiv*R1*(Cz+Cp) 1]);     % continuous time domain (S Domain)
-    % G2 = G1*Kdiv;
-    % Gdiv = c2d(G2,Ts,'tustin')                  % Discrete time domain (Z Domain)
+     G1 = tf([R1*Cz 1],[Kdiv*R1*(Cz+Cp) 1]);     % continuous time domain (S Domain)
 
-%% Multplication of Plant and Divider Transfer function to get overall G(s):
+%Plant Plus Divider Transfer Function:
 
-%  G = Gvd2 * Gdiv;
+    plant_plus_divider = plant_tf * G1;
+    [y1, t1, x1] = step(plant_plus_divider); % Get info for multiple plotting on the same axis
+    [mag1, pha1, w1] = bode(plant_plus_divider); % Get info for multiple plotting on the same axis
 
-% figure(5);
-% step(G);
+    figure(3);
+    step(plant_plus_divider);
+    title('Step Response of the Plant Plus Divider');
+    legend("Plant Plus Divider")
+    [Gm_plant_plus_divider,Pm_plant_plus_divider,Wcg_plant_plus_divider,Wcp_plant_plus_divider] = margin(plant_plus_divider);
 
-%% PID controller:
-% rootlocus method or PID tuner method (yet to decide):
+%Check system controlability:
+
+%TO BE IMPLEMENTED
+##    [A B C D] = tf2ss(plant_plus_divider)
+##    rank_system = rank(ctrb(A,B))   %Ensure
+
+
+
+% Compare bode plots with different divider circuits
+% Plot to show minimal difference in Cp values
+
+    Cp2 = 1/(2*pi*fs*Rp); %New Cp value to compare with hardware value of 10e-9F
+
+% Transfer function of the Divider circuit:
+
+    G2 = tf([R1*Cz 1],[Kdiv*R1*(Cz+Cp2) 1]);
+    plant_plus_divider_2 = plant_tf * G2;
+    [y2, t2, x2] = step(plant_plus_divider_2);
+    [mag2, pha2, w2] = bode(plant_plus_divider_2);
+
+%Plot comparison of step responses
+    figure(4)
+    plot(t1,y1, 'r:');
+    hold on;
+    plot(t2,y2, 'k--');
+    hold off;
+    title('Comparison of divider circuits')
+    legend("Cp = 10e-9 (Hardware)", "Cp = 0.6e-9 (Theory)")
+
+%Check plant_plus_divider poles and zeros to simplify the model
+    %%Dominant pole approximation to simplify model
+    %https://lpsa.swarthmore.edu/PZXferStepBode/DomPole.html
+
+    [zeros, poles, k] = tf2zp(plant_plus_divider);
+
+
+%%--- OUTPUT --- %%
+##      zeros = -29036.00465
+##      poles =
+##
+##        -18857.14286 +     0.00000i
+##         -1060.94289 +  7356.99698i
+##         -1060.94289 -  7356.99698i
+##
+##      k =  35874439.46188
+
+##>> plant_plus_divider
+##
+##    Transfer function 'plant_plus_divider' from input 'u1' to output ...
+##
+##                        1902 s + 5.524e+07
+##     y1:  ----------------------------------------------
+##          5.303e-05 s^3 + 1.113 s^2 + 5052 s + 5.525e+07
+
+%Use output to determine a simpler model
+% Since the second and third poles are dominant, the first pole at s = 188857 can be ignored.
+
+% New second order model form: K/(A*s^2 + B*s + C)
+    K = 1902 * 29036;   %Set gain to match the previous transfer function
+    A = 1;
+    B = 2*1060.94289;
+    C =  7356.99698^2;
+
+    second_order = tf([K],[A B C])
+
+%Assess new second order model vs thrid order
+
+    [y3, t3, x3] = step(second_order); % Get info for multiple plotting on the same axis
+    [mag3, pha3, w3] = bode(second_order); % Get info for multiple plotting on the same axis
+
+%Plot comparison of step responses
+
+    figure(5)
+    plot(t1,y1, 'r:');
+    hold on;
+    plot(t3,y3, 'k--');
+    hold off;
+    title('Comparison second order and third order model')
+    legend("Third order model", "Second order model")
+
+%% Multiple bode plots
+
+    figure(6)
+    grid on;
+
+    subplot(2,1,1)
+    hold on;
+    semilogx(w1,20*log10(abs(mag1)));
+    semilogx(w3,20*log10(abs(mag3)));
+    hold off;
+    legend("Third order model", "Second order model");
+    title("Constructed magnitude plot for comparison of second and third order models");
+
+    subplot(2,1,2)
+    hold on;
+    semilogx(w1, pha1);
+    semilogx(w3, pha3);
+    hold off;
+    legend("Third order model", "Second order model");
+    title("Constructed phase plot for comparison of second and third order models");
+
+% Sense check that bode plots are plotting correctly.
+
+##    figure(10)
+##    bode(plant_plus_divider)
+##    title("Actual bode plot for third order model")
+##    figure(11)
+##    bode(second_order)
+##    title("Actual bode plot for second order model")
+
+%%--- TUNING  METHODS --- %%
+
+% http://faculty.mercer.edu/jenkins_he/documents/TuningforPIDControllers.pdf
+
+%From inspection values of L (dead time) and T (time constant) are:
+% L = 0.00006s
+% T = 0.00017s
+
+% Ziegler-Nichls Tuning Rules give
+
+%% P Controller (1)
+% P_1 = T/L
+% I_1 = Inf.
+% D_1 = 0
+
+%% PI COntroller (2)
+% P_2 = 0.9*T/L
+% I_2 = L/0.3
+% D_2 = 0
+
+%% PID Controller (3)
+% P_3 = 1.2*T/L
+% I_3 = 2*L
+% D_3 = 0.5*L
+
+% Read parameters
+
+   L = 0.00006;
+   T = 0.00017;
+
+%Define PID Parameters
+
+   P_1 = T/L;  %P control only
+   I_1 = 1e30;% approximately Inf
+   D_1 = 0;
+
+   P_2 = 0.9*T/L;  %PI control
+   I_2 = L/0.3;
+   D_2 = 0;
+
+   P_3 = 1.2*T/L;   %PID control
+   I_3 = 2*L;
+   D_3 = 0.5*L;
+
+
+%Define feedback transfer functions
+  s = tf('s');
+  FB_P = tf(P_1 + I_1/s + D_1*s);
+  FB_PI = tf(P_2 + I_2/s + D_2*s);
+  FB_PID = tf(P_3 + I_3/s + D_3*s);
+
+%Apply feedback loop
+
+%% CHOICE TO APPLY FEEDBACK LOOP TO SECOND OR THIRD ORDER SYSTEM
+  %Second order
+##  PID_P = feedback(second_order,FB_P);
+##  PID_PI = feedback(second_order,FB_PI);
+##  PID_PID = feedback(second_order,FB_PID);
+
+  %Third order
+  PID_P = feedback(plant_plus_divider,FB_P);
+  PID_PI = feedback(plant_plus_divider,FB_PI);
+  PID_PID = feedback(plant_plus_divider,FB_PID);
+% Get step data
+
+    [y_P, t_P, x_P] = step(PID_P);
+    [y_PI, t_PI, x_PI] = step(PID_PI);
+    [y_PID, t_PID, x_PID] = step(PID_PID);
+
+% Get bode data
+
+    [mag_P, pha_P, w_P] = bode(PID_P);
+    [mag_PI, pha_PI, w_PI] = bode(PID_PI);
+    [mag_PID, pha_PID, w_PID] = bode(PID_PID);
+
+%Plot comparison of step responses
+
+    figure(7)
+    plot(t_P,y_P, 'r:');
+    hold on;
+    plot(t_PI,y_PI, 'k--');
+    plot(t_PID,y_PID, 'b-.');
+    hold off;
+    title('Comparison P, PI and PID control')
+    legend("P control", "PI control", "PID control")
+
+
+
+
+
+%% -- OTHER FEEDBACK METHODS --
+%% Find ultimate gain with feedback
+%% Pol placement method
+
+
+%% Additional Notes (legacy code) for discrete time modelling of the converter
+
+##%% Control-to-output-voltage transfer function (Discrete time domain):
+##% Literature : Applying Digital Technology to PWM Control-Loop Designs
+##    % Discrete time modelling (Trailing Edge DPWM and sampling at interval 2(DTs < td < Ts)):
+##
+##        % phi   = (exp(A2*(Ts-td)))*(exp(A1*Duty*Ts))*exp(A2*(td-Duty*Ts));
+##        % alpha = (B1 - B2)*Vg;
+##        % gamma = (exp(A2*(Ts-td)))*alpha*Ts;
+##        % z     = tf('z');
+##        % Gvd   = (C* inv(I-((1/z)*phi))*gamma)/z
+##
+##
+##% Literature (Small-Signal Discrete-Time Modeling of Digitally Controlled PWM Converters)
+##    % Direct equation:
+##
+##     z      = tf('z');
+##     Num    = z + (Ts/(Ts- td + Cap*Resr))*((Resr/R)-((Cap*Resr)/Ts)- (((Ts-td)*Resr)/L)+(td/Ts));
+##     Dem    = z^2 - (2 - (Ts/(R*Cap)))*z + (1 - Ts/(R*Cap) + Ts^2/(L*Cap));
+##     Gvd2   = ((Vg*Ts*(Ts- td + Cap*Resr))*Num)/(L*Cap*Dem)
